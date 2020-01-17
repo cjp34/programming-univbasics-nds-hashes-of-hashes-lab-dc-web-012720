2e62b3b19fc4d8ef360ddd83b3e02145f09fd617
Carl Parm <carlparm@gmail.com>
2020-01-17 20:31:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,7 +44,7 @@ def naming_system
 
 world = {
   :label => Kingdom {
-    :sub_category => Phylum {
+    sub_category: "Phylum" {
       :sub_category => Class {
         :sub_category => Order
       }
